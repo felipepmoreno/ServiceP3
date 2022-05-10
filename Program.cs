db704cd0f222f11338567d70ef9601b666ad764c
felipepmoreno <felipepmoreno@gmail.com>
2022-05-09 21:30:28 -0300

    tentativa de correcao de url

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ namespace ServiceP3
 #if DEBUG
                     webBuilder.UseUrls("https://localhost:5002");
 #else
-                    webBuilder.UseUrls("https://service-p3.azurewebsites.net/");
+                    webBuilder.UseUrls("https://service-p3.azurewebsites.net");
 #endif
                 });
     }
